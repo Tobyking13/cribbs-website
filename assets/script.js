d57f90951b41d9d86ab136d78b9a4453377e2a3a
Toby King <tobyking13@gmail.com>
2023-02-07 12:18:49 +0000

    creates menu page

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,30 @@
-function myFunction() {
-    var mobMenu = document.getElementById("mobMenu");
-    if (mobMenu.style.display === "block") {
-      mobMenu.style.display = "none";
-    } else {
-      mobMenu.style.display = "block";
-    }
+$(document).ready(function() {
+  $('.navbar ul li a').click(function() {
+    $('html, body').animate({
+      scrollTop: $($(this).attr("href")).offset().top
+    }, 1000); // The animation will take 1000ms (1 second) to complete.
+    return false;
+  });
+});
+
+const images = document.querySelectorAll('.menu-image');
+const fullscreenOverlay = document.getElementById('fullscreen-overlay');
+const fullscreenImage = document.getElementById('fullscreen-image');
+const closeFullscreen = document.getElementById('close-fullscreen');
+
+images.forEach(image => {
+  image.addEventListener('click', function() {
+    fullscreenOverlay.style.display = 'flex';
+    fullscreenImage.src = this.src;
+  });
+});
+
+closeFullscreen.addEventListener('click', function() {
+  fullscreenOverlay.style.display = 'none';
+});
+
+fullscreenOverlay.addEventListener('click', function(e) {
+  if (e.target === this) {
+    fullscreenOverlay.style.display = 'none';
   }
+});
